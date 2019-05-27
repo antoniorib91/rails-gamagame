5995492af526c27ccc91dad9ec4f11e266c1c838
Antônio Ribeiro <antoniorib91@gmail.com>
2019-05-26 23:27:29 -0300

    Added current_game and method to prevent direct access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :prevent_access_question_without_game
+
+  def prevent_access_question_without_game
+    if current_game.nil?
+      game = Game.find_by(token: params[:token]) unless params[:token].nil?
+      set_current_game(game) unless game.nil?
+      redirect_to root_url if current_game.nil?
+    end
+  end
+
+  def set_current_game(game)
+    @current_game = game;
+  end
+
+  def current_game
+    @current_game
+  end
+
+  
 end
